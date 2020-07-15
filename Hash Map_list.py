3435451546c72e9f7fe870e744be22a70d31f5b8
Aarav Chandra <aaravchandra02@gmail.com>
2020-07-15 16:15:36 -0700

    renamed

--- Hash Map_list.py
+++ Hash Map_list.py
@@ -24,3 +24,33 @@ All keys and values will be in the range of [0, 1000000].
 The number of operations will be in the range of [1, 10000].
 Please do not use the built-in HashMap library.
 """
+
+
+class MyHashMap:
+
+    def __init__(self):
+        """
+        Initialize the data structure here.
+        """
+
+    def put(self, key: int, value: int) -> None:
+        """
+        Value will always be non-negative.
+        """
+
+    def get(self, key: int) -> int:
+        """
+        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+        """
+
+    def remove(self, key: int) -> None:
+        """
+        Removes the mapping of the specified value key if this map contains a mapping for the key
+        """
+
+
+# MyHashMap object will be instantiated and called as such:
+obj = MyHashMap()
+obj.put(key, value)
+param_2 = obj.get(key)
+obj.remove(key)
